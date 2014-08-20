0a949b67cc1081f9cd607780b82f8d26b1f1966a
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2014-08-20 13:37:50 -0400

    Add basic mongoose function

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/scheduler', function (error) {
+  if (error) {
+      console.log(error);
+  }
+  else{
+    console.log('Successfully Connected');
+  }
+});
+
+var Schema = mongoose.Schema;
+var EventSchema = new Schema({
+  name: String,
+});
+
+var Event = mongoose.model('events', EventSchema);
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  addEvent("Geoff");
   res.render('index', { title: 'Express' });
 });
+
+var addEvent = function(name){
+  new Event({name: name}).save();
+}
 
 module.exports = router;
