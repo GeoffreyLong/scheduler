974f75b87f6ebe98f44c97ffd8a3b009c26553a2
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2014-09-08 19:28:55 -0400

    Add note

--- public/javascripts/showDay.js
+++ public/javascripts/showDay.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
-  var date = new Date();
-  var hour = date.getHours();
-  var min = date.getMinutes();
-  if (hour >=6 && hour <= 22){
-    var topOffset = hour - 6;
-    topOffset += parseInt(min/15);
-
-    // May not be perfect sized
-    // Should find less hack way to do this
-    var rowSize = $('#dayTimeLabel td').height();
-    alert(rowSize);
-  }
+  // Create a table dynamically and insert the elements into it?
+  // Alternatively create dynamically in jade (probably better option)
 });
