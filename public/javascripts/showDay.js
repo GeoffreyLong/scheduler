0f59d00bc28753369ff409f916eec31ada28f704
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2014-09-08 19:27:39 -0400

    Add table logic

--- public/javascripts/showDay.js
+++ public/javascripts/showDay.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+  var date = new Date();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  if (hour >=6 && hour <= 22){
+    var topOffset = hour - 6;
+    topOffset += parseInt(min/15);
 
+    // May not be perfect sized
+    // Should find less hack way to do this
+    var rowSize = $('#dayTimeLabel td').height();
+    alert(rowSize);
+  }
 });
