8119ff2ce0aa2db474401702746705621ed2b179
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2014-09-08 21:43:43 -0400

    Add html change on event type logic

--- public/javascripts/newEvents.js
+++ public/javascripts/newEvents.js
@@ -5,6 +5,28 @@ $(document).ready(function(){
     dialogClass: "no-close",
   });
 
+  $('#eventType').change(function(){
+    var eventType = $(this).val();
+
+    $('.rToggle').addClass("hide");
+    switch(eventType){
+      case "Recurring Task":
+        $('.rToggle').removeClass("hide");
+      case "Task":
+        $('.taskToggle').removeClass("hide");
+        $('.eventToggle').addClass("hide");
+        break;
+
+      case "Recurring Event":
+        $('.rToggle').removeClass("hide");
+      case "Event":
+        $('.eventToggle').removeClass("hide");
+        $('.taskToggle').addClass("hide");
+        break;
+    }
+    //$('.eventToggle').toggleClass('hide');
+    
+  });
   
   $('#submit').click(function(e){
     e.preventDefault();
