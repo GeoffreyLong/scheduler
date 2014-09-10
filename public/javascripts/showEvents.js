e516c6fca863e60abba43ac448aff0a7405ab96a
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2014-09-09 20:24:12 -0400

    Change expand logic

--- public/javascripts/showEvents.js
+++ public/javascripts/showEvents.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $('.event').click(function(){
     $(this).toggleClass("expand");
     console.log($(this).attr("data-id"));
+    curHeight = $(this).height();
+    autoHeight = $(this).css('height', 'auto').height();
+    $(this).height(curHeight).animate({height: autoHeight}, 200);
   });
   $('.createEvent').click(function(){
     window.location.replace("http://localhost:3000/events/eventForm");    
