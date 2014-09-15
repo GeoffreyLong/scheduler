ca2ade14afc37c1c6aa870d16f029a07564c30c7
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2014-09-14 22:58:01 -0400

    Add basic implicit layout logic

--- public/javascripts/showDay.js
+++ public/javascripts/showDay.js
@@ -1,4 +1,75 @@
 $(document).ready(function(){
-  // Create a table dynamically and insert the elements into it?
-  // Alternatively create dynamically in jade (probably better option)
+  var allLayouts = [];
+  /*$.ajax({
+    type: 'POST',
+    contentType: 'application/json',
+    url: 'http://localhost:3000/event/tags',
+    async: false,
+    statusCode: {
+      200: function(data) {
+        console.log(data);
+        data.forEach(function(elm){
+          allTags.push(elm.name);
+        });
+      },
+      400: function() {
+        alert("Didn't work");
+      }
+    }
+  });*/
+
+  $("#layouts").autocomplete({
+    source: function (request, response) {
+      var result = $.ui.autocomplete.filter(allLayouts, request.term);
+      response(result);
+    },
+    minLength: 0,
+  });
+
+  var addLayout = function(){
+    var layout = $("#layouts").val();
+
+    if($.inArray(layout, allLayouts) == -1 && layout != ""){
+      var data = {};
+      data.name = layout;
+      console.log(allLayouts);
+      console.log(layout);
+      /*
+      $.ajax({
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        url: 'http://localhost:3000/event/tag/create',
+        async: false,
+        statusCode: {
+          200: function(data) {
+            allTags.push(tag);
+            console.log("successful add");
+          },
+          500: function() {
+            alert("Didn't work");
+          }
+        }
+      });
+      */
+    }
+  };
+
+  $('#addLayoutSub').click(function(){
+    var tr = $(this).parent().parent();
+    var ul = $('#currentImplicitLayout');
+
+    var minTime = tr.find('#layoutMinTime').val();
+    var maxTime = tr.find('#layoutMaxTime').val();
+
+    var prependString = '<li>';
+    prependString += tr.find('#layoutTag').val();
+    prependString += minTime;
+    prependString += maxTime;
+    prependString += '</li>';
+
+    tr.find('[type=text]').val('');
+
+    ul.prepend(prependString);
+  });
 });
